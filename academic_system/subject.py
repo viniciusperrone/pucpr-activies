54619adbe688312b3f3ee9dd601f9d96197b4f60
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-04-25 20:45:50 -0300

    created Subject class

---
+++ academic_system/subject.py
@@ -0,0 +1,12 @@
+
+"""
+    The Subject class will have following fields:
+        - Full Name
+        - Age
+        - Gender
+"""
+
+class Subject:
+    def __init__(self, name, workload):
+        self.name = name
+        self.workload = workload
