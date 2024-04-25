7fcd759066d65c78815b06509261ffe4002536f2
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-04-25 20:41:15 -0300

    created Student class

---
+++ academic_system/student.py
@@ -0,0 +1,13 @@
+
+"""
+    The Student class will have following fields:
+        - Full Name
+        - Age
+        - Gender
+"""
+
+class Student:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
