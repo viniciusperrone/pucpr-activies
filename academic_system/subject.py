1a75b5b0b8c24945357c82d562e532404339ce9a
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-04-25 22:28:48 -0300

    improviment Subject class

--- academic_system/subject.py
+++ academic_system/subject.py
@@ -9,3 +9,9 @@ class Subject:
     def __init__(self, name, workload):
         self.name = name
         self.workload = workload
+
+    def convert_to_dict(self):
+        return { "name": self.name, "workload": self.workload }
+    
+    def __str__(self) -> str:
+        return self.name
