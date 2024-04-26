aed3094cfb5f29dad388335618a688357c6dce4a
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-04-25 21:34:18 -0300

    created Teacher class

---
+++ academic_system/teacher.py
@@ -0,0 +1,16 @@
+
+"""
+    The Teacher class will have following fields:
+        - Full Name
+        - Age
+        - Gender
+        - Subject
+"""
+
+class Teacher:
+    def __init__(self, name, age, gender, subject):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.subject = subject
+        
