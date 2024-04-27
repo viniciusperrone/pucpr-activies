d1d8b35afa56238dba77841d5398cc48460f3797
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-04-27 15:29:37 -0300

    updated menu.py

--- academic_system/menu.py
+++ academic_system/menu.py
@@ -19,6 +19,14 @@ Sistema PUC
 
 """
 
+MENU_SCHOOL_CLASS_UPDATE_TEXT = """
+Sistema PUC
+
+1. Remover alunos
+2. Adicionar alunos
+3. Atualizar professor
+"""
+
 def menu_main():
     print(MENU_MAIN_TEXT)
 
@@ -30,6 +38,15 @@ def menu_main():
 
 def menu_item():
     print(MENU_MODULE_TEXT)
+
+    choose_item = int(input("Digite sua opção: "))
+
+    print("\n")
+
+    return choose_item
+
+def menu_update_school_class():
+    print(MENU_SCHOOL_CLASS_UPDATE_TEXT)
 
     choose_item = int(input("Digite sua opção: "))
 
