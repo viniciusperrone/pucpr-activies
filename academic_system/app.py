a40a367978f6318b6490c0636f471b41b5ec0dfc
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-06-05 21:11:30 -0300

    fix: hidden sidebar

--- academic_system/app.py
+++ academic_system/app.py
@@ -3,6 +3,10 @@ from PIL import Image
 import os
 
 from pages.home import home_page
+from pages.student import student_page
+from pages.tearcher import teacher_page
+from pages.subject import subject_page
+from pages.school_class import school_class_page
 
 current_dir = os.path.dirname(__file__)
 
@@ -10,7 +14,19 @@ css_path = os.path.join(current_dir, 'assets', 'css', 'global.css')
 
 st.set_page_config(
     page_title="PUCPR | Sistema Acadêmico",
-    page_icon='📚'
+    page_icon='📚',
+    initial_sidebar_state="collapsed"
+)
+
+st.markdown(
+    """
+        <style>
+            [data-testid="collapsedControl"] {
+                display: none
+            }
+        </style>
+    """,
+    unsafe_allow_html=True,
 )
 
 with open(css_path) as f:
@@ -28,3 +44,15 @@ if 'page' in query_params:
 
 if st.session_state.page == "Home":
     home_page()
+
+elif st.session_state.page == "Estudante":
+    student_page()
+
+elif st.session_state.page == "Professor":
+    teacher_page()
+
+elif st.session_state.page == "Disciplina":
+    subject_page()
+
+elif st.session_state.page == "Turma":
+    school_class_page()
